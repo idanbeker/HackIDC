cb24e7ed87f6306be75925a5c73a8164e12517a4
Ilan Peleg <ilan@gmail.com>
2016-05-06 16:38:45 +0300

    added time calc. changed to public members

--- src/TrainingDataService.java
+++ src/TrainingDataService.java
@@ -3,14 +3,12 @@ import java.util.ArrayList;
 
 import weka.core.Instance;
 
-//import weka.core.Instance;
 
-
-public class TrainingDataService {
+public class TrainingDataService  {
 	ArrayList<Squat> m_squatList;
 
 	public TrainingDataService() {
-
+		
 	}
 
 }

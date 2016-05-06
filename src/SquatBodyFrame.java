c29652a7025339f4f1c69103c08103de3739ab98
Idan <idanbek@gmail.com>
2016-05-06 14:32:23 +0300

    Added Frame class

--- src/SquatBodyFrame.java
+++ src/SquatBodyFrame.java
@@ -1,5 +1,4 @@
 
-
 import java.awt.Point;
 
 public class SquatBodyFrame {
@@ -21,36 +20,48 @@ public class SquatBodyFrame {
 	public double getBackAngle() {
 		double shoulderToAssAngle = Math.atan2(m_shoulder.getY() - m_ass.getY(), m_shoulder.getX() - m_ass.getX());
 		double kneeToAssAngle = Math.atan2(m_knee.getY() - m_ass.getY(), m_knee.getX() - m_ass.getX());
-		return shoulderToAssAngle - kneeToAssAngle;
+		return Math.toDegrees(shoulderToAssAngle - kneeToAssAngle);
+
 	}
 
 	public double getKneeBendAngle() {
 		double assToKneeAngle = Math.atan2(m_ass.getY() - m_knee.getY(), m_ass.getX() - m_knee.getX());
 		double feetToKneeAngle = Math.atan2(m_feet.getY() - m_knee.getY(), m_feet.getX() - m_knee.getX());
-		return assToKneeAngle - feetToKneeAngle;
+		return Math.toDegrees(assToKneeAngle - feetToKneeAngle);
 	}
 
 	public String toString() {
 		return "======================\n" + "Head Value= " + m_head + "\nShoulder Value= " + m_shoulder
 				+ "\nAss Value= " + m_ass + "\nFeet Value= " + m_feet + "\nBack Angle= " + this.getBackAngle()
-				+ "\nKneeBend Angle = " + this.getKneeBendAngle() + "======================\n";
+				+ "\nKneeBend Angle = " + this.getKneeBendAngle() + "\n======================\n";
 	}
 
-	public static void main() {
+	public static void main(String[] args) {
 
 		/*
 		 * a good position: shoulder : 517,826 ass: 126 ,1061 foot: 593, 1001 a
 		 * bad position: shoulder: 458,594 ass: 186,906 foot: 587, 1100
 		 */
 		Point[] goodBodyParam = new Point[5];
-		goodBodyParam[0] = new Point(504, 652);
-		goodBodyParam[1] = new Point(517, 826);
-		goodBodyParam[2] = new Point(126, 1061);
-		goodBodyParam[3] = new Point(593, 1001);
-		goodBodyParam[4] = new Point(349, 1393);
+		goodBodyParam[0] = new Point(504, 652);//Head
+		goodBodyParam[1] = new Point(517, 826);//Shoulder
+		goodBodyParam[2] = new Point(126, 1061);//Ass
+		goodBodyParam[3] = new Point(593, 1001);//Knee
+		goodBodyParam[4] = new Point(349, 1393);//Feet
 		SquatBodyFrame goodBodyFrame = new SquatBodyFrame(goodBodyParam[0], goodBodyParam[1], goodBodyParam[2],
 				goodBodyParam[3], goodBodyParam[4]);
 		System.out.println(goodBodyFrame);
+
+		Point[] testAngleParam = new Point[5];
+		testAngleParam[0] = new Point(0, 0);//Head
+		testAngleParam[1] = new Point(1, 0);//Shoulder
+		testAngleParam[2] = new Point(0, 0);//Ass
+		testAngleParam[3] = new Point(0, 1);//Knee
+		testAngleParam[4] = new Point(1, 0);//Feet
+
+		SquatBodyFrame testAngleFrame = new SquatBodyFrame(testAngleParam[0], testAngleParam[1], testAngleParam[2],
+				testAngleParam[3], testAngleParam[4]);
+		System.out.println(testAngleFrame);
 
 	}
 

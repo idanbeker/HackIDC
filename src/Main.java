dfb6728a8f9ff2b1ff73afa085212da80a7141cc
Idan <idanbek@gmail.com>
2016-05-07 09:16:51 +0300

    Test data

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,25 @@ import java.util.ArrayList;
 import weka.core.Instances;
 
 public class Main {
-	private static final int NUM_OF_TRAINING_INSTANCES = 1;
+	private static final int NUM_OF_TRAINING_INSTANCES = 3;
+	private static final int NUM_OF_DATA_INSTANCES = 3;
+	private static final boolean isGoodSquat[] = {true ,true,false,false};
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws Exception {
 
-		ArrayList<Squat> trainingSquats = getTrainingSquats();
-		TrainingDataService tds = new TrainingDataService(trainingSquats);
+		ArrayList<Squat> trainingSquats = getTrainingSquats("input/input",NUM_OF_TRAINING_INSTANCES);
+		ArrayList<Squat> dataSquats = getTrainingSquats("input/data" , NUM_OF_DATA_INSTANCES);
+		TrainingDataService tds = new TrainingDataService(dataSquats);
 		Instances data = tds.getTrainingData();
-		System.out.println(data);
-		for(Squat s: trainingSquats)
+		SquatPredictor sp = new SquatPredictor(trainingSquats);
+		//System.out.println(trainingData);
+		//System.out.println(data);
+		
+		for(int i = 0; i < data.numInstances();i++)
 		{
-			System.out.println(s);
+			System.out.println(data.instance(i));
+			System.out.println("Real value = " + data.instance(i).classValue());
+			System.out.println("Predicted value = " + sp.predict(data.instance(i)));
 		}
 
 
@@ -22,17 +30,19 @@ public class Main {
 		
 	}
 
-	private static ArrayList<Squat> getTrainingSquats() throws FileNotFoundException {
+	private static ArrayList<Squat> getTrainingSquats(String i_fileName, int i_numOfInstances) throws FileNotFoundException {
 		ArrayList<Squat> trainingSquats = new ArrayList<Squat>();
-		for (int j = 1; j <= NUM_OF_TRAINING_INSTANCES; j++) {
-			InputParser ip = new InputParser("input/input" + j + ".txt");
+		for (int j = 1; j <= i_numOfInstances; j++) {
+			InputParser ip = new InputParser(i_fileName + j + ".txt");
 			ArrayList<SquatFrame> squatFrames = new ArrayList<SquatFrame>();
 			ArrayList<Point> points = ip.points;
 			for (int i = 0; i < points.size(); i += 4) {
 				SquatFrame sf = new SquatFrame(points.get(i), points.get(i + 1), points.get(i + 2), points.get(i + 3));
 				squatFrames.add(sf);
 			}
-			trainingSquats.add(new Squat(squatFrames));
+			Squat squatToAdd = new Squat(squatFrames);
+			squatToAdd.setGoodSquat(isGoodSquat[j]);
+			trainingSquats.add(squatToAdd);
 		}
 		return trainingSquats;
 	}
--- src/TrainingDataService.java
+++ src/TrainingDataService.java
@@ -21,7 +21,7 @@ public class TrainingDataService {
 	{
 		FastVector squatAttributes = getAttributesVector();
 		Instances trainingData = new Instances("SquatDataSet",squatAttributes ,100 );
-		System.out.println(trainingData);
+		//sSystem.out.println(trainingData);
 		
 		for(Squat s : m_squatList)
 		{
@@ -44,7 +44,7 @@ public class TrainingDataService {
 			values[i + 3] = squatFrameList.get(i).getKneeBendAngle();
 			values[i + 4] = squatFrameList.get(i).getBackAngle();
 		}
-		values[NUM_OF_ATTRIBUTES - 1] = i_squatToParse.isGoodSquat()? 1.0: 0.0;
+		values[NUM_OF_ATTRIBUTES - 1] = i_squatToParse.isGoodSquat()? 0.0: 1.0;
 		
 		return values;
 	}

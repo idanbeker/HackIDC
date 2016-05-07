faa9826ac693c50ffe405bfb75e168b553d559e8
Idan <idanbek@gmail.com>
2016-05-07 07:08:02 +0300

    Changed file names, added toString on Point and yeah..

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,20 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class Main {
+	private static final int NUM_OF_TRAINING_INSTANCES = 1;
+	
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println("Hey");
+
+    	
         InputParser ip = new InputParser("input/input.txt");
 
         ArrayList<SquatFrame> squatFrames = new ArrayList<SquatFrame>();
         ArrayList<Point> points = ip.points;
-        for (int i = 0; i < points.size(); i+=4) {
-            SquatFrame sf = new SquatFrame(points.get(i), points.get(i+1), points.get(i+2), points.get(i+3));
+        for (int i = 0; i < points.size(); i += 4) {
+        	SquatFrame sf = new SquatFrame(points.get(i), points.get(i+1), points.get(i+2), points.get(i+3));
             squatFrames.add(sf);
+            System.out.println(sf);
+            System.out.println(sf);
         }
 
         Squat s = new Squat(squatFrames);
